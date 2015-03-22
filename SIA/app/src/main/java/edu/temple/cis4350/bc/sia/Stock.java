226eb67132599c4c113884cbc230d90f449ad2a0
Brett Crawford <brettjamescrawford@gmail.com>
2015-03-21 20:48:42 -0400

    Updated the Stock and Stocks classes, Added documentation

--- SIA/app/src/main/java/edu/temple/cis4350/bc/sia/Stock.java
+++ SIA/app/src/main/java/edu/temple/cis4350/bc/sia/Stock.java
@@ -1,49 +1,68 @@
+/*
+  Brett Crawford
+  Stock Information App
+  CIS 4350
+  Spring 2015
+ */
+
 package edu.temple.cis4350.bc.sia;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+/**
+ * A class for representing a stock
+ */
 public class Stock {
 
+    // Data to persist
     private String stockSymbol;
-    private String stockName;
     private int stockColorCode;
+    private int listPosition;
+
+    // Data that can be retrieved
+    private String stockName;
     private String stockPrice;
     private String stockChange;
     private String stockPrevClosePrice;
     private String stockOpenPrice;
     private String stockMarketCap;
 
-    public Stock(String stockSymbol, int stockColorCode) {
-        this.stockSymbol = stockSymbol;
+    /**
+     * Creates a stock object.
+     *
+     * @param stockSymbol the symbol
+     * @param stockColorCode the RGB color code
+     * @param listPosition the list position
+     */
+    public Stock(String stockSymbol, int stockColorCode, int listPosition) {
+
+        this.stockSymbol = stockSymbol.toUpperCase();
+        this.stockColorCode = stockColorCode;
+        this.listPosition = listPosition;
+
         this.stockName = "null";
-        this.stockColorCode = stockColorCode;
-        this.stockPrice = "0";
-        this.stockChange = "0";
-        this.stockPrevClosePrice = "0";
-        this.stockOpenPrice = "0";
-        this.stockMarketCap = "0";
+        this.stockPrice = "null";
+        this.stockChange = "null";
+        this.stockPrevClosePrice = "null";
+        this.stockOpenPrice = "null";
+        this.stockMarketCap = "null";
     }
 
-    public Stock(String stockSymbol, String stockName, int stockColorCode) {
-        this.stockSymbol = stockSymbol;
-        this.stockName = stockName;
-        this.stockColorCode = stockColorCode;
-        this.stockPrice = "0";
-        this.stockChange = "0";
-        this.stockPrevClosePrice = "0";
-        this.stockOpenPrice = "0";
-        this.stockMarketCap = "0";
-    }
-
+    /**
+     * Creates a stock object from a JSON representation.
+     *
+     * @param stockJSONObject the JSON representation
+     */
     public Stock(JSONObject stockJSONObject) {
         try {
             // These will be necessary
             this.stockSymbol = stockJSONObject.getString("symbol");
-            this.stockName = stockJSONObject.getString("name");
             this.stockColorCode = stockJSONObject.getInt("colorCode");
+            this.listPosition = stockJSONObject.getInt("listPosition");
 
             // These may not be necessary, as they should be populated during run-time
+            this.stockName = stockJSONObject.getString("name");
             this.stockPrice = stockJSONObject.getString("price");
             this.stockChange = stockJSONObject.getString("change");
             this.stockPrevClosePrice = stockJSONObject.getString("prevClosePrice");
@@ -55,15 +74,21 @@ public class Stock {
         }
     }
 
+    /**
+     * Returns a JSON representation of the stock object.
+     *
+     * @return the JSON representation
+     */
     public JSONObject getStockJSONObject() {
         JSONObject stockJSONObject = new JSONObject();
         try {
-            // These should be necessary
+            // These will be necessary
             stockJSONObject.put("symbol", stockSymbol);
-            stockJSONObject.put("name", stockName);
             stockJSONObject.put("colorCode", stockColorCode);
+            stockJSONObject.put("listPosition", listPosition);
 
-            // These change to often to warrant persisting the data
+            // These change too often to warrant persisting the data
+            stockJSONObject.put("name", stockName);
             stockJSONObject.put("price", stockPrice);
             stockJSONObject.put("change", stockChange);
             stockJSONObject.put("prevClosePrice", stockPrevClosePrice);
@@ -77,67 +102,166 @@ public class Stock {
         return stockJSONObject;
     }
 
+    /**
+     * Returns the stock symbol.
+     *
+     * @return the stock symbol
+     */
     public String getStockSymbol() {
         return stockSymbol;
     }
 
+    /**
+     * Sets the stock symbol.
+     *
+     * @param stockSymbol the stock symbol
+     */
     public void setStockSymbol(String stockSymbol) {
         this.stockSymbol = stockSymbol;
     }
 
-    public String getStockName() {
-        return stockName;
-    }
-
-    public void setStockName(String stockName) {
-        this.stockName = stockName;
-    }
-
+    /**
+     * Returns the stock color code.
+     *
+     * @return the stock color code
+     */
     public int getStockColorCode() {
         return stockColorCode;
     }
 
+    /**
+     * Sets the stock color code.
+     *
+     * @param stockColorCode the stock color code
+     */
     public void setStockColorCode(int stockColorCode) {
         this.stockColorCode = stockColorCode;
     }
 
+    /**
+     * Returns the list position.
+     *
+     * @return the list position
+     */
+    public int getListPosition() {
+        return listPosition;
+    }
+
+    /**
+     * Sets the list position.
+     *
+     * @param listPosition the list position
+     */
+    public void setListPosition(int listPosition) {
+        this.listPosition = listPosition;
+    }
+
+    /**
+     * Returns the stock name.
+     *
+     * @return the stock name
+     */
+    public String getStockName() {
+        return stockName;
+    }
+
+    /**
+     * Sets the stock name.
+     *
+     * @param stockName the stock name
+     */
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
+
+    /**
+     * Returns the stock price.
+     *
+     * @return the stock price
+     */
     public String getStockPrice() {
         return stockPrice;
     }
 
+    /**
+     * Sets the stock price.
+     *
+     * @param stockPrice the stock price
+     */
     public void setStockPrice(String stockPrice) {
         this.stockPrice = stockPrice;
     }
 
+    /**
+     * Returns the stock change.
+     *
+     * @return the stock change
+     */
     public String getStockChange() {
         return stockChange;
     }
 
+    /**
+     * Sets the stock change.
+     *
+     * @param stockChange the stock change
+     */
     public void setStockChange(String stockChange) {
         this.stockChange = stockChange;
     }
 
+    /**
+     * Returns the stock previous close price.
+     *
+     * @return the stock previous close price
+     */
     public String getStockPrevClosePrice() {
         return stockPrevClosePrice;
     }
 
+    /**
+     * Sets the stock previous close price.
+     *
+     * @param stockPrevClosePrice the stock previous close price
+     */
     public void setStockPrevClosePrice(String stockPrevClosePrice) {
         this.stockPrevClosePrice = stockPrevClosePrice;
     }
 
+    /**
+     * Returns the stock open price.
+     *
+     * @return the stock open price
+     */
     public String getStockOpenPrice() {
         return stockOpenPrice;
     }
 
+    /**
+     * Sets the stock open price.
+     *
+     * @param stockOpenPrice the stock open price
+     */
     public void setStockOpenPrice(String stockOpenPrice) {
         this.stockOpenPrice = stockOpenPrice;
     }
 
+    /**
+     * Returns the stock market capitalization.
+     *
+     * @return the stock market capitalization
+     */
     public String getStockMarketCap() {
         return stockMarketCap;
     }
 
+    /**
+     * Sets the stock market capitalization.
+     *
+     * @param stockMarketCap  the stock market capitalization
+     */
     public void setStockMarketCap(String stockMarketCap) {
         this.stockMarketCap = stockMarketCap;
     }
+
 }
--- SIA/app/src/main/java/edu/temple/cis4350/bc/sia/Stocks.java
+++ SIA/app/src/main/java/edu/temple/cis4350/bc/sia/Stocks.java
@@ -1,52 +1,125 @@
+/*
+  Brett Crawford
+  Stock Information App
+  CIS 4350
+  Spring 2015
+ */
+
 package edu.temple.cis4350.bc.sia;
 
 import android.util.Log;
 
 import java.util.ArrayList;
 
+/**
+ * A class that holds an ArrayList of Stock objects.
+ */
 public class Stocks {
 
     private static final String TAG = "Stocks";
 
+    /* The list of stocks */
     private ArrayList<Stock> stocks;
 
+    /**
+     * Creates a stocks object.
+     */
     public Stocks() {
+
         stocks = new ArrayList<Stock>();
     }
 
+    /**
+     * Adds a stock to the list of stocks. Checks the list of stocks
+     * for the stock symbol being added. If the stock symbol already
+     * exists the stock is not added and false is returned. Otherwise,
+     * the stock is added to the list and true is returned.
+     *
+     * @param stock the stock to add
+     * @return true on success, otherwise false
+     */
     public boolean add(Stock stock) {
         for (Stock s : stocks) {
             if (s.getStockSymbol().equals(stock.getStockSymbol())) {
                 Log.d(TAG, "Stock " + stock.getStockSymbol() + " already exists in collection");
+
                 return false;
             }
         }
         stocks.add(stock);
         Log.d(TAG, "Successfully added " + stock.getStockSymbol() + " to list");
+
         return true;
     }
 
-    public Stock get(int index) {
-        return stocks.get(index);
+    /**
+     * Returns the stock with the given list position. If the stock is
+     * not found, null is returned.
+     *
+     * @param listPosition the list position of the stock
+     * @return the stock with the given list position or null if not found
+     */
+    public Stock get(int listPosition) {
+        for (Stock s : stocks) {
+            if (s.getListPosition() == listPosition) {
+                return s;
+            }
+        }
+        Log.d(TAG, "Could not locate stock with list position " + listPosition + " in list");
+
+        return null;
     }
 
+    /**
+     * Returns the stock with the given stock symbol. If the stock is not
+     * found, null is returned.
+     *
+     * @param stockSymbol the stock symbol of the stock
+     * @return the stock with the given symbol or null if not found
+     */
     public Stock get(String stockSymbol) {
         for (Stock s : stocks) {
             if (s.getStockSymbol().equals(stockSymbol)) {
                 return s;
             }
         }
-        Log.d(TAG, "Could not locate " + stockSymbol + " in list");
+        Log.d(TAG, "Could not locate stock with stock symbol " + stockSymbol + " in list");
+
         return null;
     }
 
+    /**
+     * Returns the ArrayList of stock objects.
+     *
+     * @return the ArrayList of stocks
+     */
     public ArrayList<Stock> getArrayList() {
+
         return stocks;
     }
 
+    /**
+     * Returns the size of the list of stocks.
+     *
+     * @return the size of the stock list
+     */
     public int size() {
+
         return stocks.size();
     }
 
+    /**
+     * Returns an array of strings containing all of the stock symbols
+     * in the stock list.
+     *
+     * @return an array of stock symbol strings
+     */
+    public String[] getStockSymbolStringArray() {
+        String[] stockSymbols = new String[size()];
+        for (int i = 0; i < size(); i++) {
+            stockSymbols[i] = stocks.get(i).getStockSymbol();
+        }
 
+        return stockSymbols;
+    }
 }
