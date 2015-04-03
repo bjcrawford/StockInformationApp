19723c4e09b0a913f6c0194e3fa10a7e7a803b2e
Brett Crawford <brettjamescrawford@gmail.com>
2015-04-02 22:16:54 -0400

    Updated python parser to use a different formatting method

--- PythonParseScript/formatNASDAQ.py
+++ PythonParseScript/formatNASDAQ.py
@@ -2,4 +2,7 @@ with open("NASDAQ.txt") as input:
     with open("NASDAQ-Formatted.txt", "w") as output:
         for line in input:
             lineData = line.split("\t")
-            output.write("<item>[" + lineData[0] + "] " + lineData[1].rstrip() + "</item>\n")
+            strFill = ""
+            for i in range(len(lineData[0]), 5):
+                strFill += " "
+            output.write("<item>" + lineData[0] + strFill + " " + lineData[1].rstrip() + "</item>\n")
