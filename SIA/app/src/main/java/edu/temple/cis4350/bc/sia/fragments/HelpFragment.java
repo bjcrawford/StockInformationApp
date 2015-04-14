4ac1fffb66b568077ee1dfaf94dad236648bb1d5
Brett Crawford <brettjamescrawford@gmail.com>
2015-04-14 16:50:57 -0400

    Modified help fragment and strings to allow for html styling in text fields (bold)

--- SIA/app/src/main/java/edu/temple/cis4350/bc/sia/fragments/HelpFragment.java
+++ SIA/app/src/main/java/edu/temple/cis4350/bc/sia/fragments/HelpFragment.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import edu.temple.cis4350.bc.sia.R;
 
@@ -14,12 +16,22 @@ public class HelpFragment extends Fragment {
 
     private OnHelpFragmentInteractionListener listener;
 
+    private View view;
+
+    private TextView helpStockListDrawerOpenTextView;
+    private TextView helpStockListDrawerAddTextView;
+    private TextView helpStockListDrawerRemoveTextView;
+    private TextView helpStockListDrawerCloseTextView;
+
+    private TextView helpStockDetailPageOpenTextView;
+
+    public HelpFragment() {
+
+    }
+
     public static HelpFragment newInstance() {
         HelpFragment fragment = new HelpFragment();
         return fragment;
-    }
-
-    public HelpFragment() {
     }
 
     @Override
@@ -30,7 +42,23 @@ public class HelpFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_help, container, false);
+        view = inflater.inflate(R.layout.fragment_help, container, false);
+
+        helpStockListDrawerOpenTextView = (TextView) view.findViewById(R.id.help_stock_list_drawer_open);
+        helpStockListDrawerAddTextView = (TextView) view.findViewById(R.id.help_stock_list_drawer_add);
+        helpStockListDrawerRemoveTextView = (TextView) view.findViewById(R.id.help_stock_list_drawer_remove);
+        helpStockListDrawerCloseTextView = (TextView) view.findViewById(R.id.help_stock_list_drawer_close);
+
+        helpStockDetailPageOpenTextView = (TextView) view.findViewById(R.id.help_stock_detail_page_open);
+
+        helpStockListDrawerOpenTextView.setText(Html.fromHtml(getString(R.string.help_stock_list_drawer_open)));
+        helpStockListDrawerAddTextView.setText(Html.fromHtml(getString(R.string.help_stock_list_drawer_add)));
+        helpStockListDrawerRemoveTextView.setText(Html.fromHtml(getString(R.string.help_stock_list_drawer_remove)));
+        helpStockListDrawerCloseTextView.setText(Html.fromHtml(getString(R.string.help_stock_list_drawer_close)));
+
+        helpStockDetailPageOpenTextView.setText(Html.fromHtml(getString(R.string.help_stock_detail_page_open)));
+
+        return view;
     }
 
     public void onSomeAction(Uri uri) {
